35ddb619c0596359850df39322a884da1e88c1b9
jack <jack.kau@flex-solver.com.sg>
2020-01-09 12:36:12 +0800

    angular ques

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
 	title = 'Flex-Solver';
+
+	options = [
+		{
+			name: 'A',
+			value: 1
+		},
+		{
+			name: 'B',
+			value: 2
+		},
+		{
+			name: 'C',
+			value: 3
+		},
+	]
+
+
+	onClick() {
+		alert('You got it')
+	}
+	
 }
